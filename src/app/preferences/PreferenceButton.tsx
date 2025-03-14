28761467535c2ac8ea965d1a7140898bda54fe3f
Kevin Lim <klim0071@student.monash.edu>
2025-03-15 01:12:29 +1100

    Kevin: Added hover colour changes for preference buttons and used react state to simulate multiple selections

--- src/app/preferences/PreferenceButton.tsx
+++ src/app/preferences/PreferenceButton.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 interface PreferenceButtonProps{ 
     label: string;
 }
 
 const PreferenceButton: React.FC<PreferenceButtonProps> = ({label}) => {
+    const [isActive, setIsActive] = useState(false);
+
+    const handleClick = () => {
+        setIsActive(!isActive);
+    };
+
     return (
-        <button className="bg-white px-6 py-3 rounded-md shadow-md font-medium text-black">
+        <button className={`px-6 py-3 rounded-md shadow-md font-medium text-black hover:bg-gray-300
+                            ${isActive ? "bg-gray-300" : "bg-white"}`} onClick={handleClick}>
             {label}
         </button>
     );
