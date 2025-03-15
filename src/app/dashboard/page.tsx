2ffd75f12da84c939118f17baecee4be7a12bb9a
sleo0040 <sleo0040@student.monash.edu>
2025-03-15 17:16:24 +1100

    Added back-forth button for dashboard-reward. View all button on rewards page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,42 +1,65 @@
 'use client'
 
+import { useRouter } from 'next/navigation';
+import { motion } from 'framer-motion';
 import TaskCard from '@/components/TaskCard';
 import ProfileCard from '@/components/ProfileCard'
 
 export default function Dashboard() {
+    const router = useRouter();
+
+    const handleNextClick = () => {
+        router.push("/dashboard/rewards");
+    }
+
     return (
-        <div className='min-h-screen bg-pink-100 p-4 sm:p-6'>
-            <ProfileCard
-                username='Kevin Lim'
-            />
-            <div className='flex justify-between items-center mb-4'>
-                <h3 className='text-black text-xl font-bold'> Daily Tasks</h3>
-                <button className='bg-[#F50B57] text-white px-4 py-2 rounded-lg'>Reroll</button>
+        <>
+            {/* Reward Button */}
+            <div className="fixed top-12 right-12">
+                <motion.button className="bg-[#F50B57] text-white px-4 py-2 rounded-lg"
+                        whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
+                        whileTap={{ scale: 0.9 }}
+                        onClick={handleNextClick}>
+                    View Rewards
+                </motion.button>
             </div>
-            <TaskCard 
-                title="Visit Flinders St Station" 
-                type="Attraction" 
-                criteria="Take a photo" 
-                exp={69} 
-            />
-            <TaskCard 
-                title="Eat at Lune Croissanterie" 
-                type="Breakfast" 
-                criteria="Take a photo" 
-                exp={420} 
-            />
-            <TaskCard 
-                title="Eat at Hakata Gensuke" 
-                type="Lunch" 
-                criteria="Take a photo" 
-                exp={6969} 
-            />
-            <TaskCard 
-                title="Visit Monash University" 
-                type="Attraction" 
-                criteria="Take a photo" 
-                exp="Bonus" 
-            />
-        </div>
-    )
+                
+            {/* Profile Card */}
+            <div className='min-h-screen bg-pink-100 p-4 sm:p-6'>
+                <ProfileCard
+                    username='Kevin Lim'
+                />
+                <div className='flex justify-between items-center mb-4'>
+                    <h3 className='text-black text-xl font-bold'> Daily Tasks</h3>
+                    <button className='bg-[#F50B57] text-white px-4 py-2 rounded-lg'>Reroll</button>
+                </div>
+
+                {/* Task Cards */}
+                <TaskCard 
+                    title="Visit Flinders St Station" 
+                    type="Attraction" 
+                    criteria="Take a photo" 
+                    exp={69} 
+                />
+                <TaskCard 
+                    title="Eat at Lune Croissanterie" 
+                    type="Breakfast" 
+                    criteria="Take a photo" 
+                    exp={420} 
+                />
+                <TaskCard 
+                    title="Eat at Hakata Gensuke" 
+                    type="Lunch" 
+                    criteria="Take a photo" 
+                    exp={6969} 
+                />
+                <TaskCard 
+                    title="Visit Monash University" 
+                    type="Attraction" 
+                    criteria="Take a photo" 
+                    exp="Bonus" 
+                />
+            </div>
+        </>
+    );
 }
