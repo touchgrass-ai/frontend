0b3e4b7b54f05b581a51b7792e87e6fd075a631a
Kevin Lim <klim0071@student.monash.edu>
2025-03-15 23:10:02 +1100

    Kevin: Added top padding to navbar so that it doesnt overlap with other elements and changed design of settings page.

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -11,7 +11,6 @@ export default function Settings() {
   const [newUsername, setNewUsername] = useState("");
   const [newProfilePic, setNewProfilePic] = useState<File | null>(null);
   const [newProfilePicName, setNewProfilePicName] = useState("");
-  const [selectedSetting, setSelectedSetting] = useState<"profile" | "preferences" | null>(null);
   const [preferences, setPreferences] = useState<string[]>([]);
 
   const router = useRouter();
@@ -58,82 +57,62 @@ export default function Settings() {
   };
 
   return (
-    <div className="min-h-screen bg-[#FAE0E7] p-4 sm:p-6 flex flex-col md:flex-row">
-      <div className="w-full md:w-1/4 bg-white p-4 rounded-lg shadow-md space-y-4 mb-4 md:mb-0">
-        <h1 className="text-4xl font-bold mb-4 text-black">Settings</h1>
-        <button
-          onClick={() => setSelectedSetting("profile")}
-          className="bg-[#FF3F3F] mt-4 text-white px-4 py-2 rounded-lg shadow hover:bg-[#FF6262] transition w-full"
-        >
-          Edit Profile
-        </button>
-        <button
-          onClick={() => setSelectedSetting("preferences")}
-          className="bg-[#FF3F3F] mt-2 text-white px-4 py-2 rounded-lg shadow hover:bg-[#FF6262] transition w-full"
-        >
-          Edit Preferences
-        </button>
-      </div>
-      <div className="w-full md:w-3/4 bg-white p-4 rounded-lg shadow-md ml-0 md:ml-4 flex flex-col">
-        {selectedSetting === "profile" && (
-          <>
-            <ProfileCard username={username} profileImage={profilePic} />
-            <form onSubmit={handleSubmit} className="bg-white p-4 rounded-lg shadow-md">
-              <div className="mb-4">
-                <label htmlFor="username" className="block text-sm font-medium text-black text-semi-bold">
-                  Change username by entering new username in field below.
-                </label>
-                <input
-                  type="text"
-                  id="username"
-                  value={newUsername}
-                  onChange={handleNewUsernameChange}
-                  className="text-black mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 focus:bg-gray-200 sm:text-sm"
-                />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="profilePic" className="block text-sm font-medium text-black semi-bold mb-2">
-                  Profile Picture
-                </label>
-                <input
-                  type="file"
-                  id="profilePic"
-                  accept="image/*"
-                  onChange={handleProfilePicChange}
-                  className="hidden text-black"
-                />
-                <label
-                  htmlFor="profilePic"
-                  className="mt-2 bg-[#FF3F3F] text-white px-4 py-2 rounded-lg shadow hover:bg-red-600 transition cursor-pointer"
-                >
-                  Upload File Here
-                </label>
-                {newProfilePicName && (
-                  <p className="mt-2 text-sm text-gray-600">{newProfilePicName}</p>
-                )}
-              </div>
-              <button
-                type="submit"
-                className="mt-6 bg-[#FF6262] text-white px-4 py-2 rounded-lg shadow hover:bg-[#FF3F3F] transition"
-              >
-                Save Changes
-              </button>
-            </form>
-          </>
-        )}
-        {selectedSetting === "preferences" && (
-          <div className="flex-grow">
-            <h2 className="text-2xl font-bold mb-4 text-black">Edit Preferences</h2>
-            <p className="text-lg font-semibold mb-4 text-black"> The following are the preferences that you have previously chosen.</p>
-            <div className="flex flex-wrap gap-2">
-              {preferences.map((preference, index) => (
-                <div key={index} className="bg-gray-200 text-black px-4 py-2 rounded-lg shadow">
-                  {preference}
-                </div>
-              ))}
-            </div>
+    <div className="min-h-screen bg-[#FAE0E7] p-4 sm:p-6 flex flex-col">
+      <h1 className="text-4xl font-bold mb-4 text-black">Settings</h1>
+      <div className="bg-white p-4 rounded-lg shadow-md flex flex-col">
+        <ProfileCard username={username} profileImage={profilePic} />
+        <form onSubmit={handleSubmit} className="bg-white p-4 rounded-lg shadow-md">
+          <div className="mb-4">
+            <label htmlFor="username" className="block text-sm font-medium text-black text-semi-bold">
+              Change username by entering new username in field below.
+            </label>
+            <input
+              type="text"
+              id="username"
+              value={newUsername}
+              onChange={handleNewUsernameChange}
+              className="text-black mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 focus:bg-gray-200 sm:text-sm"
+            />
           </div>
-        )}
+          <div className="mb-4">
+            <label htmlFor="profilePic" className="block text-sm font-medium text-black semi-bold mb-2">
+              Profile Picture
+            </label>
+            <input
+              type="file"
+              id="profilePic"
+              accept="image/*"
+              onChange={handleProfilePicChange}
+              className="hidden text-black"
+            />
+            <label
+              htmlFor="profilePic"
+              className="mt-2 bg-[#FF3F3F] text-white px-4 py-2 rounded-lg shadow hover:bg-red-600 transition cursor-pointer"
+            >
+              Upload File Here
+            </label>
+            {newProfilePicName && (
+              <p className="mt-2 text-sm text-gray-600">{newProfilePicName}</p>
+            )}
+          </div>
+          <button
+            type="submit"
+            className="mt-6 bg-[#FF6262] text-white px-4 py-2 rounded-lg shadow hover:bg-[#FF3F3F] transition"
+          >
+            Save Changes
+          </button>
+        </form>
+        <div className="flex-grow mt-8">
+          <h2 className="text-2xl font-bold mb-4 text-black">Edit Preferences</h2>
+          <p className="text-lg font-semibold mb-4 text-black"> The following are the preferences that you have previously chosen.</p>
+          <div className="flex flex-wrap gap-2">
+            {preferences.map((preference, index) => (
+              <div key={index} className="bg-gray-200 text-black px-4 py-2 rounded-lg shadow">
+                {preference}
+              </div>
+            ))}
+          </div>
+        </div>
         <button
           onClick={handleNextClick}
           className="mt-4 bg-[#FF3F3F] text-white px-4 py-2 rounded-lg shadow hover:bg-red-600 transition self-end"
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,8 +1,6 @@
-
-
-import { motion } from 'motion/react'
-import Image from 'next/image'
-import { useRouter } from 'next/navigation'
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export default function NavBar() {
   const router = useRouter();
@@ -12,42 +10,47 @@ export default function NavBar() {
   };
 
   return (
-    <nav className="fixed bottom-0 inset-x-0 bg-white border-t border-gray-200 flex justify-around py-3">
-      <motion.div
-        className='flex flex-col items-center'
-        whileHover={{
-          scale: 1.01,
-          transition: { duration: 0.2 },
-        }}
-        whileTap={{ scale: 0.9 }}
-        onClick={()=>handleNavigation('/dashboard')}
-        >
-        <Image src='/home.svg' alt='home' width={25} height={25} className='w-8 h-8'/>
-        <div className='text-black text-xs'>Home</div>
-      </motion.div>
-      <motion.div className='flex flex-col items-center'
-        whileHover={{
-          scale: 1.01,
-          transition: { duration: 0.2 },
-        }}
-        whileTap={{ scale: 0.9 }}
-        onClick={()=>handleNavigation('/dashboard/rewards')}
-        >
-        <Image src='/gift.png' alt='rewards' width={20} height={20} className='w-8 h-8'/>
-        <div className='text-black text-xs'>Rewards</div>
-      </motion.div>
-      <motion.div className='flex flex-col items-center'
-        whileHover={{
-          scale: 1.01,
-          transition: { duration: 0.2 },
-        }}
-        whileTap={{ scale: 0.9 }}
-        onClick={()=>handleNavigation('/dashboard/settings')}
-        >
-        <Image src='/settings.svg' alt='settings' width={25} height={25} className='w-8 h-8'/>
-        <div className='text-black text-xs'>Settings</div>
-      </motion.div>
-    </nav>
-  )
-
+    <>
+      <div className="pb-20"> {/* Added padding-bottom to ensure content is not covered */}
+        <nav className="fixed bottom-0 inset-x-0 bg-white border-t border-gray-200 flex justify-around py-3">
+          <motion.div
+            className='flex flex-col items-center'
+            whileHover={{
+              scale: 1.01,
+              transition: { duration: 0.2 },
+            }}
+            whileTap={{ scale: 0.9 }}
+            onClick={() => handleNavigation('/dashboard')}
+          >
+            <Image src='/home.svg' alt='home' width={25} height={25} className='w-8 h-8'/>
+            <div className='text-black text-xs'>Home</div>
+          </motion.div>
+          <motion.div
+            className='flex flex-col items-center'
+            whileHover={{
+              scale: 1.01,
+              transition: { duration: 0.2 },
+            }}
+            whileTap={{ scale: 0.9 }}
+            onClick={() => handleNavigation('/dashboard/rewards')}
+          >
+            <Image src='/gift.png' alt='rewards' width={20} height={20} className='w-6 h-6'/>
+            <div className='text-black text-xs'>Rewards</div>
+          </motion.div>
+          <motion.div
+            className='flex flex-col items-center'
+            whileHover={{
+              scale: 1.01,
+              transition: { duration: 0.2 },
+            }}
+            whileTap={{ scale: 0.9 }}
+            onClick={() => handleNavigation('/dashboard/settings')}
+          >
+            <Image src='/settings.svg' alt='settings' width={25} height={25} className='w-8 h-8'/>
+            <div className='text-black text-xs'>Settings</div>
+          </motion.div>
+        </nav>
+      </div>
+    </>
+  );
 }
